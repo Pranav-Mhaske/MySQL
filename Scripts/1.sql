0f37e4376ab1755548c025bd3b5ca597890dae9e
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-06-02 23:39:25 +0530

    DDL Commands completed

--- Scripts/1.sql
+++ Scripts/1.sql
@@ -3,6 +3,9 @@
 -- Create Database
 CREATE DATABASE sales;
 
+-- Use DAtabase
+Use sales;
+
 -- Create Table
 CREATE TABLE stores
 (
@@ -11,26 +14,44 @@ CREATE TABLE stores
 );
 
 -- Insert Some Records
+INSERT INTO stores
+VALUES
+(1,"store_xyz"),
+(2,"store_abc");
+
+-- This will work because there is no NOT NULL constraint on store_name
 INSERT INTO stores(store_id)
 VALUES
 (3);
 
--- Create Table
+-- Create Table with Constraints
 CREATE TABLE stores_new
 (
-	store_id INT UNIQUE,
-    store_name VARCHAR(200) NOT NULL
+	store_id INT UNIQUE, -- UNIQUE constraint ensures that all values in the column are different.
+    store_name VARCHAR(200) NOT NULL -- NOT NULL constraint ensures that the column cannot have NULL values.
 );
 
--- Insert Some Data
+-- Insert Some Data into stores_new
 INSERT INTO stores_new
 VALUES
-(1,"store_xyz");
+(1,"store_xyz"),
+(2,"store_abc"),
+(3,"store_pqr");
 
+-- Will not work because of the NOT NULL constraint on store_name
+INSERT INTO stores_new(store_id)
+VALUES
+(4);
 
 -- ALTER COMMAND
 ALTER TABLE stores_new
 ADD COLUMN store_city VARCHAR(200);
 RENAME COLUMN store_city to store_location;
 
+
+--Drop command
+DROP TABLE stores; -- This will delete the entire table and all its data.
+
+--Truncate command
+TRUNCATE TABLE stores_new; -- This will delete all the data in the table but keep the structure of the table intact. 
 
