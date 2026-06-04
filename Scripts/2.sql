a8933c8b6f77714894bf11211768ea9baa9b5156
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-06-04 22:16:56 +0530

    Droping existing data

---
+++ Scripts/2.sql
@@ -0,0 +1,6 @@
+-- Commands to drop tables if they exist.
+DROP TABLE IF EXISTS fact_sales;
+DROP TABLE IF EXISTS dim_date;
+DROP TABLE IF EXISTS dim_customer;
+DROP TABLE IF EXISTS dim_product;
+DROP TABLE IF EXISTS dim_store;
