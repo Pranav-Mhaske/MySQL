7d33e6ceba9113953103625128e59f933f52a57f
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-06-06 00:28:54 +0530

    Like clause

--- Scripts/3.sql
+++ Scripts/3.sql
@@ -27,3 +27,21 @@ FROM
 	dim_customer 
 WHERE 
 	(gender = 'F') AND ((country = 'France') OR (join_date > '2022-01-01'));
+
+-- LIKE
+-- 1)
+SELECT 
+	* 
+FROM 
+	dim_customer
+WHERE 
+	first_name LIKE 'T%';
+-- 2)
+SELECT 
+	* 
+FROM 
+	dim_customer
+WHERE 
+	first_name LIKE 'T__f%y';
+
+
