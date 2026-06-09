c33e458dc031d97bcc417e8f41e7f7d11a30f728
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-06-09 23:36:16 +0530

    Right join

--- Scripts/4.sql
+++ Scripts/4.sql
@@ -21,3 +21,15 @@ LEFT JOIN
 		o.cust_id = c.id;
 
 --Left join will return all records from the left table (orders) and the matched records from the right table (customers). If there is no match, the result is NULL on the right side.
+
+-- RIGHT JOIN
+SELECT 
+	*
+FROM 
+	orders o 
+RIGHT JOIN 
+	customers c 
+	ON 
+		o.cust_id = c.id;
+
+--Right join will return all records from the right table (customers) and the matched records from the left table (orders). If there is no match, the result is NULL on the left side.
