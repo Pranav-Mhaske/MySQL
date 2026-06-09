1d63798538abbb33e63d0b9db992de37ea72b677
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-06-09 23:35:30 +0530

    Left join

--- Scripts/4.sql
+++ Scripts/4.sql
@@ -9,3 +9,15 @@ INNER JOIN
 		o.cust_id = c.id;
 
 -- Inner join will return only the records that have matching values in both tables. If there is no match, the record will not be included in the result set.
+
+-- LEFT JOIN
+SELECT 
+	*
+FROM 
+	orders o 
+LEFT JOIN 
+	customers c 
+    ON 
+		o.cust_id = c.id;
+
+--Left join will return all records from the left table (orders) and the matched records from the right table (customers). If there is no match, the result is NULL on the right side.
