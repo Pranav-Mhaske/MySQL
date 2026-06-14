5d1158af432cebab73d9646dc6a2fb9c035685e4
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-06-14 23:45:36 +0530

    Numberic Transformations

--- Scripts/6.sql
+++ Scripts/6.sql
@@ -1,1 +1,11 @@
 -- TRANSFORMATIONS
+
+-- Numeric Transformations
+SELECT 
+	unit_price * 0.90 AS discounted_price,
+    unit_price + 10 as taxed_price,
+    unit_price / 10 as fractioned_price,
+    ROUND(unit_price,1) as rounded_price,
+    unit_price * unit_price as multiply_price
+FROM 
+	dim_product;
