f99525d19787ca48a9d0e82529bac336aeb18010
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-06-18 22:35:54 +0530

    Conditional Statement 2

--- Scripts/7.sql
+++ Scripts/7.sql
@@ -9,3 +9,16 @@ SELECT
     END AS price_category
 FROM 
 	dim_product;
+
+
+-- 2)
+SELECT 
+	*,
+    CASE 
+    WHEN unit_price <= 100 AND category = 'clothing' THEN 'affordable'
+    WHEN unit_price <= 200 AND category = 'clothing' THEN 'normal'
+    WHEN unit_price > 200 AND category = 'clothing' THEN 'expensive (but not for you!)'
+    ELSE CONCAT('Not for ',category) 
+    END AS price_category
+FROM 
+	dim_product;
