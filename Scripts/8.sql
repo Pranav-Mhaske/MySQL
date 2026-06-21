5ebd3d135f1f8e153309f505f610ec26d02a1f6e
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-06-21 19:44:39 +0530

    Window function explanation

--- Scripts/8.sql
+++ Scripts/8.sql
@@ -5,3 +5,19 @@ SELECT
     SUM(unit_price) OVER(ORDER BY launch_date)
 FROM 
 	dim_product;
+
+-- 2) FRAMES
+SELECT 
+	*,
+    SUM(unit_price) OVER(ORDER BY launch_date ROWS BETWEEN unbounded preceding AND current row)
+FROM 
+	dim_product;
+
+SELECT 
+	*,
+    SUM(unit_price) OVER(ORDER BY launch_date ROWS BETWEEN unbounded preceding AND unbounded following)
+FROM 
+	dim_product;
+
+-- unbounded preceding means all the previous values till my current row
+-- unbounded following means all the next values from my current row
