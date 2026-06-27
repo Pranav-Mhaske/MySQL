940c7a018edb55a5613c0dc98c4e7947110fc546
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-06-27 15:13:45 +0530

    CTE Example

--- Scripts/10.sql
+++ Scripts/10.sql
@@ -17,3 +17,19 @@ WHERE
 )
 SELECT * FROM cte_table_2
 WHERE product_name = 'Figure Method'
+
+
+--2
+WITH TotalSales AS (
+    SELECT salesperson_id, SUM(amount) AS total_revenue
+    FROM sales
+    GROUP BY salesperson_id
+),
+TopPerformers AS (
+    SELECT salesperson_id
+    FROM TotalSales
+    WHERE total_revenue > 100000
+)
+SELECT emp.employee_name
+FROM employees emp
+JOIN TopPerformers tp ON emp.id = tp.salesperson_id;
