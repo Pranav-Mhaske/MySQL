da96956fbff803e492e1c1f042d618e58d6bf456
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-07-17 23:26:37 +0530

    Running total

--- Scripts/16.sql
+++ Scripts/16.sql
@@ -1,3 +1,14 @@
 SELECT MAX(salary) AS SecondHighestSalary 
 FROM Employee 
 WHERE salary < (SELECT MAX(salary) FROM Employee);
+
+
+SELECT
+    product_id,
+    sale_date,
+    amount,
+    SUM(amount) OVER (
+        PARTITION BY product_id
+        ORDER BY sale_date
+    ) AS running_total
+FROM Sales;
