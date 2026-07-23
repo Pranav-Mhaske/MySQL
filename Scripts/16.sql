9049b27d7a7244efec843c4eb28adce955cc3b37
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-07-23 22:30:32 +0530

    Assignnment

--- Scripts/16.sql
+++ Scripts/16.sql
@@ -48,3 +48,9 @@ FROM
     STATION
 WHERE
     (MOD(ID, 2) = 0);
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(LOWER(CITY), 1) IN ('a', 'e', 'i', 'o', 'u')
+  AND RIGHT(LOWER(CITY), 1) IN ('a', 'e', 'i', 'o', 'u');
