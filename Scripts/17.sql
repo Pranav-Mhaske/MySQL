199d07388a51841867eec304f6656eccf72647a9
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-07-28 23:03:14 +0530

    Hacker Rank 2nd

--- Scripts/17.sql
+++ Scripts/17.sql
@@ -1,8 +1,15 @@
 --Hacker Rank
 
+--1
+
 SELECT
     *
 FROM
     CITY
 WHERE
     (COUNTRYCODE = 'USA') AND (POPULATION >= 100000);
+
+--2
+
+select NAME from CITY where POPULATION > 120000 AND COUNTRYCODE = 'USA';
+
